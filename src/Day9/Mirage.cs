f8f4b04583ea79cda4d783d6495d448d5d9cfd8d
NH S <noud.savenije@capgemini.com>
2023-12-09 08:10:27 +0100

    a bit neater

--- src/Day9/Mirage.cs
+++ src/Day9/Mirage.cs
@@ -2,92 +2,45 @@ public static class Day9
 {
     public static void Solve1()
     {
-        
         List<List<int>> histories = ParseInput();
         List<int> results = [];
         foreach(List<int> seq in histories)
         {
-            int extrapolation = - 1;
-            bool allZeroes = false;
-            List<List<int>> seqs = [];
-            seqs.Add([]);
-            seqs[0] = seq;
-            int depth = 0;
-            while(!allZeroes)
-            {
-                seqs.Add(new List<int>());
-                depth++;
-                for(int i = 0; i < seqs[depth - 1].Count - 1; i++)
-                {
-                    //parent
-                    List<int> parent = seqs[depth - 1];
-                    // Console.WriteLine(string.Join(',', parent));
-                    //compare all pairs
-                    seqs[depth].Add(parent[i+1] - parent[i]);
-                }
-                // Console.WriteLine($"@depth {depth}: {string.Join(',', seqs[depth])}");
-                allZeroes = seqs[depth].All(c => c == 0);
-            }
-            // All zeroes
+            List<List<int>> seqs = BuildPyramid(seq);
+            int depth = seqs.Count - 1;
             // Add the padding value and loop back to the start
             seqs[depth].Add(0);
             depth--;
             while(depth >= 0)
             {
-                List<int> parent = seqs[depth + 1];
-                seqs[depth].Add(seqs[depth].Last() + parent.Last());
+                seqs[depth].Add(seqs[depth].Last() + seqs[depth + 1].Last());
                 // Console.WriteLine(string.Join(',', seqs[depth]) + " <> " + string.Join(',', seqs[depth + 1])); 
                 depth--;
             }
-            extrapolation = seqs[0].Last();
+            int extrapolation = seqs[0].Last();
             results.Add(extrapolation);
         }
-        Console.WriteLine(string.Join(',',results));
+        // Console.WriteLine(string.Join(',',results));
         Console.WriteLine(results.Sum());
     }
     public static void Solve2()
     {
-                List<List<int>> histories = ParseInput();
+        List<List<int>> histories = ParseInput();
         List<int> results = [];
         foreach(List<int> seq in histories)
         {
-            int extrapolation = - 1;
-            bool allZeroes = false;
-            List<List<int>> seqs = [];
-            seqs.Add([]);
-            seqs[0] = seq;
-            int depth = 0;
-            while(!allZeroes)
-            {
-                seqs.Add(new List<int>());
-                depth++;
-                for(int i = 0; i < seqs[depth - 1].Count - 1; i++)
-                {
-                    //parent
-                    List<int> parent = seqs[depth - 1];
-                    // Console.WriteLine(string.Join(',', parent));
-                    //compare all pairs
-                    seqs[depth].Add(parent[i+1] - parent[i]);
-                }
-                // Console.WriteLine($"@depth {depth}: {string.Join(',', seqs[depth])}");
-                allZeroes = seqs[depth].All(c => c == 0);
-            }
-            // All zeroes
+            List<List<int>> seqs = BuildPyramid(seq);
+            int depth = seqs.Count - 1;
             // Add the padding value and loop back to the start
             seqs[depth].Add(0);
             depth--;
             while(depth >= 0)
             {
-                List<int> parent = seqs[depth + 1];
-
-                int toAdd = seqs[depth].First() + parent.First();
-                // Console.WriteLine("ToAdd " + toAdd);
-                Console.WriteLine($"@dept {depth}: Me {string.Join(',', seqs[depth])}  parent {string.Join(',', seqs[depth + 1])}"); 
-                seqs[depth].Add(seqs[depth].First() - parent.Last());
-                
+                seqs[depth].Add(seqs[depth].First() - seqs[depth + 1].Last());
+                // Console.WriteLine($"@dept {depth}: Me {string.Join(',', seqs[depth])}  parent {string.Join(',', seqs[depth + 1])}"); 
                 depth--;
             }
-            extrapolation = seqs[0].Last();
+            int extrapolation = seqs[0].Last();
             results.Add(extrapolation);
         }
         Console.WriteLine(string.Join(',',results));
@@ -104,5 +57,30 @@ public static class Day9
             res.Add(line.Split(" ").Select(int.Parse).ToList());
         }
         return res;
+    }
+
+    static List<List<int>> BuildPyramid(List<int> sequence)
+    {
+        List<List<int>> pyramid = [];
+        pyramid.Add([]);
+        pyramid[0] = sequence;
+        int depth = 0;
+        bool allZeroes = false;
+        while(!allZeroes)
+        {
+            pyramid.Add(new List<int>());
+            depth++;
+            for(int i = 0; i < pyramid[depth - 1].Count - 1; i++)
+            {
+                //parent
+                List<int> parent = pyramid[depth - 1];
+                // Console.WriteLine(string.Join(',', parent));
+                //compare all pairs
+                pyramid[depth].Add(parent[i+1] - parent[i]);
+            }
+            // Console.WriteLine($"@depth {depth}: {string.Join(',', pyramid[depth])}");
+            allZeroes = pyramid[depth].All(c => c == 0);
+        }
+        return pyramid;
     }
 }
