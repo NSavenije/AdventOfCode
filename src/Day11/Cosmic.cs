eca79fa95eeef13ad0bb6369e834ca29b7e48f4c
NH S <noud.savenije@capgemini.com>
2023-12-11 12:01:50 +0100

    Slightly cleaner

--- src/Day11/Cosmic.cs
+++ src/Day11/Cosmic.cs
@@ -6,20 +6,15 @@ public static class Day11
 {
     public static void Solve1()
     {
-        var galaxies = ParseInput1("src/Day11/11.in");
-        long sum = 0;
-        foreach((int x1,int y1) in galaxies)
-        {
-            foreach((int x2,int y2) in galaxies)
-            {
-                sum += Math.Abs(x2 - x1) + Math.Abs(y2 - y1);
-            }
-        }
-        sum /= 2;
-        Console.WriteLine(sum);
+        Solve(2);
     }
 
     public static void Solve2()
+    {
+        Solve(1000000);
+    }
+
+    static void Solve(int expansion = 2)
     {
         using StreamReader sr = new("src/Day11/11.in");
         string line;
@@ -68,6 +63,7 @@ public static class Day11
         }
 
         long sum = 0;
+        List<int> dists = [];
         foreach((int x1,int y1) in galaxies)
         {
             foreach((int x2,int y2) in galaxies)
@@ -75,77 +71,21 @@ public static class Day11
                 if (x2 < x1 || ( x2 == x1 && y2 <= y1))
                     continue;
 
-                int xBig, xSmall, yBig, ySmall;
-                if (x1 >= x2)
-                {
-                    xBig = x1; xSmall = x2;
-                }
-                else{
-                    xBig = x2; xSmall = x1;
-                }
-                if (y1 >= y2)
-                {
-                    yBig = y1; ySmall = y2;
-                }
-                else{
-                    yBig = y2; ySmall = y1;
-                }
-                int dist = 0;
-                for(int x = xSmall + 1; x < xBig; x++)
-                {
-                    if (cols.Contains(x))
-                        dist += 1000000;
-                }
-                for(int y = ySmall + 1; y < yBig; y++)
-                {
-                    if (rows.Contains(y))
-                        dist += 1000000;
-                }
-                dist += Math.Abs(x2 - x1) + Math.Abs(y2 - y1) - 1;
+                int low, high;
+                low = Math.Min(x1, x2); high = Math.Max(x1, x2);
+                int xs = Enumerable.Range(low, high - low).Intersect(cols).Count() * (expansion - 1);
+                low = Math.Min(y1, y2); high = Math.Max(y1, y2);
+                int ys = Enumerable.Range(low, high - low).Intersect(rows).Count() * (expansion - 1);
+                int dist = Math.Abs(x2 - x1) + Math.Abs(y2 - y1) + xs + ys;
                 sum += dist;
+                dists.Add(dist);
+                // Console.WriteLine($"({x1},{y1})-({x2},{y2}): {dist}({Math.Abs(x2 - x1)},{Math.Abs(y2 - y1)},{xs},{ys})");
             }
         }
+        Console.WriteLine(string.Join(',',cols));
+        Console.WriteLine(string.Join(',',rows));
         Console.WriteLine(sum);
-    }
-
-    static List<(int,int)> ParseInput1(string filePath)
-    {
-        using StreamReader sr = new(filePath);
-        string line;
-
-        List<string> input = [];
-        while ((line = sr.ReadLine()) != null)
-        {
-            input.Add(line);
-            if (line.All(c => c == '.'))
-                input.Add(line);
-        }
-        List<string> transposed = TransposeRowsToColumns(input);
-        List<string> result = [];
-        foreach(string s in transposed)
-        {
-            result.Add(s);
-            if (s.All(c => c == '.'))
-                result.Add(s);
-        }
-        result = TransposeRowsToColumns(result);
-
-        int[,] universe = new int[result[0].Length,result.Count];
-        int counter = 0;
-        List<(int x,int y)> galaxies = [];
-        for(int x = 0; x < universe.GetLength(0); x++)
-            for(int y = 0; y < universe.GetLength(1); y++)
-            {
-                if (result[y][x] == '#')
-                {
-                    galaxies.Add((x, y));
-                    universe[x,y] = counter++;
-                }
-                else
-                    universe[x,y] = result[y][x];
-            }
-
-        return galaxies;
+        
     }
 
     static List<string> TransposeRowsToColumns(List<string> rows)
