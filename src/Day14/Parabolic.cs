2a7f0196fae375a0bd10bf4fd9ab9223202d5f91
NH S <noud.savenije@capgemini.com>
2023-12-15 08:39:37 +0100

    Somewhat less chaotic 14

--- src/Day14/Parabolic.cs
+++ src/Day14/Parabolic.cs
@@ -7,55 +7,66 @@ public static class Day14
 {
     public static void Solve1()
     {
-        string filePath = "src/Day14/14.in";
-        var input = File.ReadAllLines(filePath);
-        // x is col, y is row
-        char[,] grid = new char[input[0].Length,input.Length];
-        for(int i = 0; i < input.Length; i++)
-        {
-            string line = input[i];
-            // i = row = y; j = col = x
-            for (int j = 0; j < line.Length; j++)
-            {
-                // Reverse
-                grid[j,i] = line[j];
-            }
-        }
-        long sum = 0;
-        // we only go up
-        for(int x = 0; x < grid.GetLength(0); x++)
-        {
-            
-            //now we are checking 1 column
-            for(int y = 0; y < grid.GetLength(1) - 1 ; y++)
-            {
-                char cur = grid[x, y];
-                char next = grid[x, y + 1];
-                if (next == 'O' && cur == '.')
-                {
-                    for (int z = y + 1; z > 0; z--)
-                    {
-                        char up = grid[x,z - 1];
-                        if (up == '.')
-                        {
-                            grid[x,z] = '.';
-                            grid[x,z - 1] = 'O';
-                        }
-                        if (up == '#' || up == 'O')
-                            break;
-                    }
-                }
-            }
-            for(int y = 0; y < grid.GetLength(1); y++)
-            {
-                if (grid[x,y] == 'O')
-                    sum += grid.GetLength(1) - y;
-            }
-        }
-        Console.WriteLine(sum);
+        char[,] grid = ParseInput();
+        North(grid);
+        Console.WriteLine(CountLoad(grid));
     }
 
     public static void Solve2()
+    {
+        char[,] grid = ParseInput();
+
+        int round = 0;
+        List<int> loads = [];
+        List<int> hashes = [];
+
+        while (round < 400) // How do I know when a cycle is possible
+        {
+            Cycle(grid);
+            hashes.Add(Hash(grid));
+            loads.Add(CountLoad(grid));
+            round++;
+        }
+
+        (int tortoise, int hare) = FindCycle(hashes);
+        
+        // FLoyd's tortoise and hare without a linked list improv...
+        
+        int length = hare - tortoise;
+        int offset = (1_000_000_000 - hare) % length - 1;
+        Console.WriteLine(loads[tortoise + offset]);
+
+    }
+
+    static (int,int) FindCycle(List<int> hashes)
+    {
+        int tortoise = 0;
+        bool foundCycle = false;
+
+        while (!foundCycle)
+        {
+            int hare;
+            for (hare = tortoise + 1; hare < hashes.Count; hare++)
+            {
+                if (hashes[tortoise] == hashes[hare] && IsCycle(hashes, tortoise, hare))
+                {
+                    return (tortoise, hare);
+                }
+            }
+            tortoise++;
+        }
+        return (-1,-1);
+    }
+
+    static void Cycle(char[,] grid)
+    {
+        North(grid);
+        West(grid);
+        South(grid);
+        East(grid);
+    }
+
+    static char[,] ParseInput()
     {
         string filePath = "src/Day14/14.in";
         var input = File.ReadAllLines(filePath);
@@ -66,51 +77,12 @@ public static class Day14
             string line = input[i];
             // i = row = y; j = col = x
             for (int j = 0; j < line.Length; j++)
-            {
-                // Reverse
                 grid[j,i] = line[j];
-            }
         }
-
-        int round = 0;
-        List<int> loads = [];
-        List<int> hashes = [];
-
-        while (round < 1100) 
-        {
-            Cycle(grid);
-            hashes.Add(Hash(grid));
-            loads.Add(CountLoad(grid));
-            round++;
-        } 
-        
-        // FLoyd's tortoise and hare without a linked list improv...
-        int tortoise = 0;
-        int hare = 1;
-        bool foundCycle = false;
-
-        while (!foundCycle)
-        {
-            for (hare = tortoise + 1; hare < hashes.Count; hare++)
-            {
-                if (hashes[tortoise] == hashes[hare] && IsCycle(hashes, tortoise, hare))
-                {
-                    foundCycle = true;
-                    break;
-                }
-            }
-            if (foundCycle)
-                break;
-            tortoise++;
-        }
-
-        int length = hare - tortoise;
-        int offset = (1_000_000_000 - hare) % length - 1;
-        Console.WriteLine(loads[tortoise + offset]);
-        
+        return grid;
     }
 
-    static void Cycle(char[,] grid)
+    static void North(char[,] grid)
     {
         // North 
         for(int x = 0; x < grid.GetLength(0); x++)
@@ -137,7 +109,10 @@ public static class Day14
                 }
             }
         }
-        // West rows
+    }
+
+    static void West(char[,] grid)
+    {
         for(int y = 0; y < grid.GetLength(1); y++)
         {   
             //walk right
@@ -163,7 +138,10 @@ public static class Day14
                 }
             }
         }
-        // South 
+    }
+
+    static void South(char[,] grid)
+    {
         for(int x = 0; x < grid.GetLength(0); x++)
         {   
             //walk up
@@ -188,8 +166,11 @@ public static class Day14
                 }
             }
         }
-        // East rows
-        for(int y = 0; y < grid.GetLength(1); y++)
+    }
+
+    static void East(char[,] grid)
+    {
+         for(int y = 0; y < grid.GetLength(1); y++)
         {   
             //walk left
             for(int x = grid.GetLength(0) - 1; x > 0 ; x--)
