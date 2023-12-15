0a68f060f1a1c38343b38c0b23c0448462eddc68
NH S <noud.savenije@capgemini.com>
2023-12-15 15:08:47 +0100

    Now with linked list!

--- src/Day14/Parabolic.cs
+++ src/Day14/Parabolic.cs
@@ -18,17 +18,17 @@ public static class Day14
 
         int round = 0;
         List<int> loads = [];
-        List<int> hashes = [];
+        LinkedList<int> hashes = new();
 
-        while (round < 400) // How do I know when a cycle is possible
+        while (round < 1000) // How do I know when a cycle is possible
         {
             Cycle(grid);
-            hashes.Add(Hash(grid));
+            hashes.AddLast(Hash(grid));
             loads.Add(CountLoad(grid));
             round++;
         }
 
-        (int tortoise, int hare) = FindCycle(hashes);
+        (int tortoise, int hare) = Floyd(hashes);
         
         // FLoyd's tortoise and hare without a linked list improv...
         
@@ -38,24 +38,51 @@ public static class Day14
 
     }
 
-    static (int,int) FindCycle(List<int> hashes)
-    {
-        int tortoise = 0;
-        bool foundCycle = false;
-
-        while (!foundCycle)
+    static (int,int) Floyd(LinkedList<int> x0) {
+        // """Floyd's cycle detection algorithm."""
+        // # Main phase of algorithm: finding a repetition x_i = x_2i.
+        // # The hare moves twice as quickly as the tortoise and
+        // # the distance between them increases by 1 at each step.
+        // # Eventually they will both be inside the cycle and then,
+        // # at some point, the distance between them will be
+        // # divisible by the period λ.
+        var tortoise = x0.Head.Next; // # f(x0) is the element/node next to x0.
+        var hare = tortoise.Next;
+        while (tortoise != hare)
         {
-            int hare;
-            for (hare = tortoise + 1; hare < hashes.Count; hare++)
-            {
-                if (hashes[tortoise] == hashes[hare] && IsCycle(hashes, tortoise, hare))
-                {
-                    return (tortoise, hare);
-                }
-            }
-            tortoise++;
+            tortoise = tortoise.Next;
+            hare = hare.Next.Next;
         }
-        return (-1,-1);
+    
+        // # At this point the tortoise position, ν, which is also equal
+        // # to the distance between hare and tortoise, is divisible by
+        // # the period λ. So hare moving in cycle one step at a time, 
+        // # and tortoise (reset to x0) moving towards the cycle, will 
+        // # intersect at the beginning of the cycle. Because the 
+        // # distance between them is constant at 2ν, a multiple of λ,
+        // # they will agree as soon as the tortoise reaches index μ.
+
+        // # Find the position μ of first repetition.    
+        int mu = 0;
+        tortoise = x0.Head;
+        while (tortoise != hare)
+        {
+            tortoise = tortoise.Next;
+            hare = hare.Next;  // # Hare and tortoise move at same speed
+            mu += 1;
+        }
+    
+        // # Find the length of the shortest cycle starting from x_μ
+        // # The hare moves one step at a time while tortoise is still.
+        // # lam is incremented until λ is found.
+        int lam = 1;
+        hare = tortoise.Next;
+        while (tortoise != hare){
+            hare = hare.Next;
+            lam += 1;
+        }
+
+        return (lam, mu);
     }
 
     static void Cycle(char[,] grid)
@@ -224,8 +251,51 @@ public static class Day14
         }
         return sum;
     }
+}
 
-    static bool IsCycle(List<int> hashes, int a, int b)
-        => Enumerable.Range(0, b - a).All(j => hashes[a + j] == hashes[b + j]);
+public class Node<T>
+{
+    public T Data { get; set; }
+    public Node<T> Next { get; set; }
+
+    public Node(T data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+public class LinkedList<T>
+{
+    public Node<T> Head { get; private set; }
+    public int Count { get; private set; }
+
+    public LinkedList()
+    {
+        Head = null;
+        Count = 0;
+    }
+
+    public void AddLast(T data)
+    {
+        Count++;
+        Node<T> newNode = new Node<T>(data);
+
+        if (Head == null)
+        {
+            Head = newNode;
+            Head.Next = Head; 
+        }
+        else
+        {
+            Node<T> current = Head;
+            while (current.Next != Head) 
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+            newNode.Next = Head; 
+        }
+    }
 }
 
