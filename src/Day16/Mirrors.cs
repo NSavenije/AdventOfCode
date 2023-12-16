28d19508879042bfd55459b667388968c63c4dc4
NH S <noud.savenije@capgemini.com>
2023-12-16 08:28:49 +0100

    Small cleanup 16

--- src/Day16/Mirrors.cs
+++ src/Day16/Mirrors.cs
@@ -6,139 +6,52 @@ public static class Day16
     {
         char[,] maze = ParseInput("src/Day16/16.in");
         int xs = maze.GetLength(0), ys = maze.GetLength(1);
-        HashSet<(int,int)>[,] energy = BuilldEmptyEnergize(xs, ys);
-
-        //Start top left, going Right
-        (int x, int y) dir = (1,0);
-        int x = 0, y = 0;
-        Laser laser = new(x,y,dir);
-        RunLaser(laser);
-        
-        int score = 0;
-        for(int ity = 0; ity < ys; ity++)
-        {
-            StringBuilder sb = new();
-            for(int itx = 0; itx < xs; itx++)
-            {
-                if (energy[itx,ity].Count > 0)
-                    score++;
-
-                // sb.Append(energy[itx,ity].Count > 0 ? '#' : '.');
-                sb.Append(maze[itx,ity]);
-
-            }
-            Console.WriteLine(sb.ToString());
-        }
-
+        int score = SolveMaze(xs, ys, 0, 0, (1,0), maze);
         Console.WriteLine(score);
-        
-        void RunLaser(Laser l)
-        {
-            // Console.WriteLine($"{l}");
-            if (energy[l.X,l.Y].Contains(l.Dir))
-            {
-                // Console.WriteLine($"HI");
-
-                return;
-
-            }
-            energy[l.X,l.Y].Add(l.Dir);
-
-            switch (maze[l.X,l.Y])
-            {
-                case '/':
-                    if (l.Dir == (1,0)) l.Dir = (0,-1);
-                    else if (l.Dir == (-1,0)) l.Dir = (0,1);
-                    else if (l.Dir == (0,1)) l.Dir = (-1,0);
-                    else if (l.Dir == (0,-1)) l.Dir = (1,0);
-                    break;
-                case '\\':
-                    if (l.Dir == (1,0)) l.Dir = (0,1);
-                    else if (l.Dir == (-1,0)) l.Dir = (0,-1);
-                    else if (l.Dir == (0,1)) l.Dir = (1,0);
-                    else if (l.Dir == (0,-1)) l.Dir = (-1,0);
-                    break;
-                case '-':
-                    if (l.Dir.x == 0) // Up or Down
-                    {
-                        l.Dir = (-1,0);
-                        if (l.X < xs - 1)
-                            RunLaser(new(l.X + 1, l.Y, (1,0)));
-                    }
-                    break;
-                case '|':
-                    if (l.Dir.y == 0) // Left or Right
-                    {
-                        l.Dir = (0,-1);
-                        if (l.Y < ys - 1)
-                            RunLaser(new(l.X, l.Y + 1, (0,1)));
-                    }
-                    break;
-                default:
-                    maze[l.X,l.Y] = '#';
-                    break;
-            }
-            l.X += l.Dir.x;
-            l.Y += l.Dir.y;
-            if (l.X >= xs || l.X < 0 || l.Y >= ys || l.Y < 0)
-                return;
-            RunLaser(l);
-        }  
     }
     public static void Solve2()
     {
         char[,] maze = ParseInput("src/Day16/16.in");
         int xs = maze.GetLength(0), ys = maze.GetLength(1);
+
         int maxScore = 0;
-        
-        int score = 0;
-        for(int itx = 0; itx < xs; itx++)
+        int score;
+
+        for (int itx = 0; itx < xs; itx++)
         {
-            score = DoIt(xs,ys,itx,0,(0,1),maze);
+            score = SolveMaze(xs,ys,itx,0,(0,1),maze);
             maxScore = Math.Max(score, maxScore);
-            score = DoIt(xs,ys,itx,ys-1,(0,-1),maze);
+            score = SolveMaze(xs,ys,itx,ys-1,(0,-1),maze);
             maxScore = Math.Max(score, maxScore);
         }
-        for(int ity = 0; ity < xs; ity++)
+        for (int ity = 0; ity < xs; ity++)
         {
-            score = DoIt(xs,ys,0,ity,(1,0),maze);
+            score = SolveMaze(xs,ys,0,ity,(1,0),maze);
             maxScore = Math.Max(score, maxScore);
-            score = DoIt(xs,ys,xs-1,0,(-1,0),maze);
+            score = SolveMaze(xs,ys,xs-1,0,(-1,0),maze);
             maxScore = Math.Max(score, maxScore);
         }
         Console.WriteLine(maxScore);
     }
 
-    static int DoIt(int xs, int ys, int x, int y, (int x, int y) dir, char[,] maze)
+    static int SolveMaze(int xs, int ys, int x, int y, (int x, int y) dir, char[,] maze)
     {
         HashSet<(int,int)>[,] energy = BuilldEmptyEnergize(xs, ys);
-        //Start top, going Down
         Laser laser = new(x,y,dir);
         RunLaser(laser);
         
         int score = 0;
         for(int iy = 0; iy < ys; iy++)
-        {
             for(int ix = 0; ix < xs; ix++)
-            {
                 if (energy[ix,iy].Count > 0)
                     score++;
-
-            }
-        }
-
         return score;
 
         void RunLaser(Laser l)
         {
-            // Console.WriteLine($"{l}");
             if (energy[l.X,l.Y].Contains(l.Dir))
-            {
-                // Console.WriteLine($"HI");
-
                 return;
-
-            }
+                
             energy[l.X,l.Y].Add(l.Dir);
 
             switch (maze[l.X,l.Y])
@@ -172,7 +85,6 @@ public static class Day16
                     }
                     break;
                 default:
-                    // maze[l.X,l.Y] = '#';
                     break;
             }
             l.X += l.Dir.x;
