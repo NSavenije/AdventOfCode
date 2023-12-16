d5db6f09f66ecf7b3f133b59d54152b2a625d541
NH S <noud.savenije@capgemini.com>
2023-12-16 08:19:40 +0100

    16 2

--- src/Day16/Mirrors.cs
+++ src/Day16/Mirrors.cs
@@ -87,7 +87,100 @@ public static class Day16
     }
     public static void Solve2()
     {
+        char[,] maze = ParseInput("src/Day16/16.in");
+        int xs = maze.GetLength(0), ys = maze.GetLength(1);
+        int maxScore = 0;
         
+        int score = 0;
+        for(int itx = 0; itx < xs; itx++)
+        {
+            score = DoIt(xs,ys,itx,0,(0,1),maze);
+            maxScore = Math.Max(score, maxScore);
+            score = DoIt(xs,ys,itx,ys-1,(0,-1),maze);
+            maxScore = Math.Max(score, maxScore);
+        }
+        for(int ity = 0; ity < xs; ity++)
+        {
+            score = DoIt(xs,ys,0,ity,(1,0),maze);
+            maxScore = Math.Max(score, maxScore);
+            score = DoIt(xs,ys,xs-1,0,(-1,0),maze);
+            maxScore = Math.Max(score, maxScore);
+        }
+        Console.WriteLine(maxScore);
+    }
+
+    static int DoIt(int xs, int ys, int x, int y, (int x, int y) dir, char[,] maze)
+    {
+        HashSet<(int,int)>[,] energy = BuilldEmptyEnergize(xs, ys);
+        //Start top, going Down
+        Laser laser = new(x,y,dir);
+        RunLaser(laser);
+        
+        int score = 0;
+        for(int iy = 0; iy < ys; iy++)
+        {
+            for(int ix = 0; ix < xs; ix++)
+            {
+                if (energy[ix,iy].Count > 0)
+                    score++;
+
+            }
+        }
+
+        return score;
+
+        void RunLaser(Laser l)
+        {
+            // Console.WriteLine($"{l}");
+            if (energy[l.X,l.Y].Contains(l.Dir))
+            {
+                // Console.WriteLine($"HI");
+
+                return;
+
+            }
+            energy[l.X,l.Y].Add(l.Dir);
+
+            switch (maze[l.X,l.Y])
+            {
+                case '/':
+                    if (l.Dir == (1,0)) l.Dir = (0,-1);
+                    else if (l.Dir == (-1,0)) l.Dir = (0,1);
+                    else if (l.Dir == (0,1)) l.Dir = (-1,0);
+                    else if (l.Dir == (0,-1)) l.Dir = (1,0);
+                    break;
+                case '\\':
+                    if (l.Dir == (1,0)) l.Dir = (0,1);
+                    else if (l.Dir == (-1,0)) l.Dir = (0,-1);
+                    else if (l.Dir == (0,1)) l.Dir = (1,0);
+                    else if (l.Dir == (0,-1)) l.Dir = (-1,0);
+                    break;
+                case '-':
+                    if (l.Dir.x == 0) // Up or Down
+                    {
+                        l.Dir = (-1,0);
+                        if (l.X < xs - 1)
+                            RunLaser(new(l.X + 1, l.Y, (1,0)));
+                    }
+                    break;
+                case '|':
+                    if (l.Dir.y == 0) // Left or Right
+                    {
+                        l.Dir = (0,-1);
+                        if (l.Y < ys - 1)
+                            RunLaser(new(l.X, l.Y + 1, (0,1)));
+                    }
+                    break;
+                default:
+                    // maze[l.X,l.Y] = '#';
+                    break;
+            }
+            l.X += l.Dir.x;
+            l.Y += l.Dir.y;
+            if (l.X >= xs || l.X < 0 || l.Y >= ys || l.Y < 0)
+                return;
+            RunLaser(l);
+        }
     }
 
     static char[,] ParseInput(string filePath)
