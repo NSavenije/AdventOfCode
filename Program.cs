393a9f07f5279497169544c2c4e9804f1c8aecb1
NH S <noud.savenije@capgemini.com>
2023-12-16 10:28:33 +0100

    Day16

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
     {
         int day = 1;
         int part = 1;
-        int currentDay = 13;
+        int currentDay = 16;
         if (args.Length == 1 && args[0] == "A")
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
